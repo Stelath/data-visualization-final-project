a2a7ced735704ac029ce8cb34987560b465b43cd
Alex <alex.k.korte@gmail.com>
2024-11-17 23:57:32 -0500

    Turn off eslint for now

--- eslint.config.js
+++ eslint.config.js
@@ -1,3 +1,32 @@
+// import js from '@eslint/js'
+// import globals from 'globals'
+// import reactHooks from 'eslint-plugin-react-hooks'
+// import reactRefresh from 'eslint-plugin-react-refresh'
+// import tseslint from 'typescript-eslint'
+
+// export default tseslint.config(
+//   { ignores: ['dist'] },
+//   {
+//     extends: [js.configs.recommended, ...tseslint.configs.recommended],
+//     files: ['**/*.{ts,tsx}'],
+//     languageOptions: {
+//       ecmaVersion: 2020,
+//       globals: globals.browser,
+//     },
+//     plugins: {
+//       'react-hooks': reactHooks,
+//       'react-refresh': reactRefresh,
+//     },
+//     rules: {
+//       ...reactHooks.configs.recommended.rules,
+//       'react-refresh/only-export-components': [
+//         'warn',
+//         { allowConstantExport: true },
+//       ],
+//     },
+//   },
+// )
+
 import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
@@ -23,6 +52,16 @@ export default tseslint.config(
         'warn',
         { allowConstantExport: true },
       ],
+      // Add these rules to ignore the specific errors
+      '@typescript-eslint/no-unused-vars': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/ban-ts-comment': 'off',
+      '@typescript-eslint/no-unsafe-member-access': 'off',
+      '@typescript-eslint/no-unsafe-call': 'off',
+      '@typescript-eslint/no-unsafe-assignment': 'off',
+      '@typescript-eslint/no-unsafe-argument': 'off',
+      '@typescript-eslint/no-unsafe-return': 'off',
+      'react-hooks/exhaustive-deps': 'off',
     },
   },
 )

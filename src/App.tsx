0c1af67166e5ae354c01e74fc28f31d0b13288b7
nschnab <132092269+nschnab@users.noreply.github.com>
2024-11-17 23:44:27 -0500

    css

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,20 @@ import EyeColorDonutChart from '@/components/eye-color-donut-chart';
 import EyeColorRatioChart from '@/components/eye-color-ratio-chart';
 import GenderDonutChart from '@/components/gender-donut-chart';
 import StateChoroplethMap from '@/components/state-choropleth-map';
+import './App.css'; // Import a CSS file for styling
 
 function App() {
   return (
     <div>
       <h1>Missing Persons Data Visualizations</h1>
-      <GenderDonutChart />
-      <EyeColorDonutChart />
-      <EyeColorRatioChart />
-      <StateChoroplethMap />
-      {/* <CountyChoroplethMap /> */}
+      <StateChoroplethMap /> {/* Keep the map at the top */}
+      <div className="chart-container">
+        <GenderDonutChart />
+        <EyeColorDonutChart />
+        <EyeColorRatioChart />
+      </div>
     </div>
   );
 }
 
-export default App
+export default App;

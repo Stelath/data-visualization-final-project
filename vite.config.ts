649c721e59ee8aa4f93f4413d85590825ddac013
Alex <korte@clemson.edu>
2024-12-03 07:02:05 -0500

    Fix misaligned base

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/data-visualization-final-project",
+  base: "./data-visualization-final-project",
   plugins: [react()],
   resolve: {
     alias: {
